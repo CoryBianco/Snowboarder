8de59e14e434a48fce3b1ffa4cc93dc3eab2ba00
Cory Bianco <corybdev@gmail.com>
2022-10-22 17:49:11 -0400

    final

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -7,14 +7,20 @@ public class CrashDetector : MonoBehaviour {
 
     [SerializeField] float delay = .5f;
     [SerializeField] ParticleSystem crashEffect;
+    [SerializeField] AudioClip crashSFX;
+    bool playedSound = false;
 
     void ReloadScene() {
+        playedSound = false;
         SceneManager.LoadScene(0);
     }
 
     void OnTriggerEnter2D(Collider2D other) {
-        if (other.tag == "Ground") {
+        if (other.tag == "Ground" && !playedSound) {
+            playedSound = true;
+            FindObjectOfType<***REMOVED***>().DisableControls();
             crashEffect.Play();
+            GetComponent<AudioSource>().PlayOneShot(crashSFX);
             Invoke("ReloadScene", delay);
         }
     }
