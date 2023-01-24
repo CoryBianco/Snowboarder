e5f875c4a43a96b755f2d212a2259b6b4933ac2a
Cory Bianco <39772555+CoryBianco@users.noreply.github.com>
2023-01-24 09:07:38 -0500

    Create README.md

--- Assets/Scripts/DustTrail.cs
+++ Assets/Scripts/DustTrail.cs
@@ -6,13 +6,13 @@ public class DustTrail : MonoBehaviour {
     [SerializeField] ParticleSystem snowEffect;
 
     private void OnCollisionEnter2D(Collision2D other) {
-        if (other.***REMOVED***.tag == "Ground") {
+        if (other.gameObject.tag == "Ground") {
             snowEffect.Play();
         }
     }
 
     private void OnCollisionExit2D(Collision2D other) {
-        if (other.***REMOVED***.tag == "Ground") {
+        if (other.gameObject.tag == "Ground") {
             snowEffect.Stop();
         }
     }
